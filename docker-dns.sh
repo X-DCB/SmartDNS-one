c12a75f49cd08196e331985cb8a652bf3a2b9815
Dexter Cellona Banawon <28706220+X-DCB@users.noreply.github.com>
2019-05-13 16:25:48 +0800

    Add files via upload

--- docker-dns.sh
+++ docker-dns.sh
@@ -4,7 +4,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt update
 apt install docker-ce -y
-mkdir /etc/_configs
+[ -d /etc/_configs ] || mkdir /etc/_configs
 echo 'bind-dynamic
 bogus-priv
 domain-needed
