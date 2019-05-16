2f85979b086eaed8fcfc473eb153021ac52321a6
Dexter Cellona Banawon <28706220+X-DCB@users.noreply.github.com>
2019-05-16 16:41:23 +0800

    Add files via upload

--- docker-dns.sh
+++ docker-dns.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+if [[ `cat /etc/apt/sources.list` =~ docker ]];then
 apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable";fi
 apt update
 apt install docker-ce -y
 [ -d /etc/_configs ] || mkdir /etc/_configs
