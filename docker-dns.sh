bea326d2d9f0ae98f599139bfb2c985477bfd42a
Dexter Cellona Banawon <28706220+X-DCB@users.noreply.github.com>
2019-05-18 02:51:19 +0800

    Add files via upload

--- docker-dns.sh
+++ docker-dns.sh
@@ -1,99 +1,25 @@
 #!/bin/bash
 while [[ ! $sqx =~ Y|y|N|n ]]; do
 	read -p "Shareable RP: [Y/y] [N/n] " sqx;done
+if [[ ! `which docker` ]]; then
 apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt update
 apt-cache policy docker-ce
-apt install docker-ce -y
+apt install docker-ce -y; fi
 [ -d /etc/_configs ] || mkdir /etc/_configs
 IP=$(wget -qO- ipv4.icanhazip.com)
-docker service ls || docker swarm init --advertise-addr $IP
-echo 'bind-dynamic
-bogus-priv
-domain-needed
-log-facility=-
-local-ttl=60
-conf-dir=/etc/dnsmasq.d/,*.conf' > /etc/_configs/dnsmasq.conf
-echo 'address=/akadns.net/IPADD
-address=/akam.net/IPADD
-address=/akamai.com/IPADD
-address=/akamai.net/IPADD
-address=/akamaiedge.net/IPADD
-address=/akamaihd.net/IPADD
-address=/akamaistream.net/IPADD
-address=/akamaitech.net/IPADD
-address=/akamaitechnologies.com/IPADD
-address=/akamaitechnologies.fr/IPADD
-address=/akamaized.net/IPADD
-address=/edgekey.net/IPADD
-address=/edgesuite.net/IPADD
-address=/srip.net/IPADD
-address=/footprint.net/IPADD
-address=/level3.net/IPADD
-address=/llnwd.net/IPADD
-address=/edgecastcdn.net/IPADD
-address=/cloudfront.net/IPADD
-address=/netflix.com/IPADD
-address=/netflix.net/IPADD
-address=/nflximg.net/IPADD
-address=/nflxvideo.net/IPADD
-address=/nflxso.net/IPADD
-address=/nflxext.com/IPADD
-address=/hulu.com/IPADD
-address=/huluim.com/IPADD
-address=/hbonow.com/IPADD
-address=/hbogo.com/IPADD
-address=/hbo.com/IPADD
-address=/amazon.com/IPADD
-address=/amazon.co.uk/IPADD
-address=/amazonvideo.com/IPADD
-address=/crackle.com/IPADD
-address=/pandora.com/IPADD
-address=/vudu.com/IPADD
-address=/blinkbox.com/IPADD
-address=/abc.com/IPADD
-address=/fox.com/IPADD
-address=/theplatform.com/IPADD
-address=/nbc.com/IPADD
-address=/nbcuni.com/IPADD
-address=/ip2location.com/IPADD
-address=/pbs.org/IPADD
-address=/warnerbros.com/IPADD
-address=/southpark.cc.com/IPADD
-address=/cbs.com/IPADD
-address=/brightcove.com/IPADD
-address=/cwtv.com/IPADD
-address=/spike.com/IPADD
-address=/go.com/IPADD
-address=/mtv.com/IPADD
-address=/mtvnservices.com/IPADD
-address=/playstation.net/IPADD
-address=/uplynk.com/IPADD
-address=/maxmind.com/IPADD
-address=/disney.com/IPADD
-address=/disneyjunior.com/IPADD
-address=/xboxlive.com/IPADD
-address=/lovefilm.com/IPADD
-address=/turner.com/IPADD
-address=/amctv.com/IPADD
-address=/sho.com/IPADD
-address=/mog.com/IPADD
-address=/wdtvlive.com/IPADD
-address=/beinsportsconnect.tv/IPADD
-address=/beinsportsconnect.net/IPADD
-address=/fig.bbc.co.uk/IPADD
-address=/open.live.bbc.co.uk/IPADD
-address=/sa.bbc.co.uk/IPADD
-address=/www.bbc.co.uk/IPADD
-address=/crunchyroll.com/IPADD
-address=/ifconfig.co/IPADD
-address=/omtrdc.net/IPADD' | \
-sed -e "s/IPADD/$IP/g" > \
-/etc/_configs/sni-dns.conf
+DNUL="/dev/null"
+CONFDIR="/etc/_configs"
+GITMINE="https://raw.githubusercontent.com/X-DCB/SmartDNS-one/master"
+docker service ls 2> $DNUL || docker swarm init --advertise-addr $IP
+docker service rm dnsx 2> $DNUL
 wget \
 https://raw.githubusercontent.com/X-DCB/netflix-proxy/master/docker-sniproxy/sniproxy.conf.template \
--qO /etc/_configs/sniproxy.conf
-wget https://raw.githubusercontent.com/X-DCB/SmartDNS-one/master/squid.conf -qO- | sed -e "s/XIP/$IP/g" | ([[ $sqx =~ N|n ]] && sed -e "s/#http/http/g" || cat) > /etc/_configs/squid.conf
-wget https://raw.githubusercontent.com/X-DCB/SmartDNS-one/master/docker.yaml -qO- | docker stack deploy -c - dnsx
+-qO $CONFDIR/sniproxy.conf
+wget $GITMINE/sni-dns.conf -qO $CONFDIR/
+wget $GITMINE/dnsmasq.conf -qO $CONFDIR/
+wget $GITMINE/squid.conf -qO $CONFDIR/
+wget $GITMINE/docker.yaml -qO- | docker stack up -c - dnsx
+docker service update $(docker service ls | grep squid | cut -d ' ' -f 1) --args $sqx
