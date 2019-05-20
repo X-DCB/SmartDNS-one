ebcb85466f0f6afb1cc9cc9485a8d01388a45e6c
Dexter Cellona Banawon <28706220+X-DCB@users.noreply.github.com>
2019-05-21 01:35:40 +0800

    Add files via upload

--- docker-dns.sh
+++ docker-dns.sh
@@ -8,18 +8,22 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(
 apt update
 apt-cache policy docker-ce
 apt install docker-ce -y; fi
-[ -d /etc/_configs ] || mkdir /etc/_configs
 IP=$(wget -qO- ipv4.icanhazip.com)
 DNUL="/dev/null"
 CONFDIR="/etc/_configs"
 GITMINE="https://raw.githubusercontent.com/X-DCB/SmartDNS-one/master"
 docker service ls 2> $DNUL || docker swarm init --advertise-addr $IP
 docker stack rm dnsx 2> $DNUL
-wget \
-https://raw.githubusercontent.com/X-DCB/netflix-proxy/master/docker-sniproxy/sniproxy.conf.template \
--qO $CONFDIR/sniproxy.conf
-wget $GITMINE/sni-dns.conf -qO $CONFDIR/sni-dns.conf
-wget $GITMINE/dnsmasq.conf -qO $CONFDIR/dnsmasq.conf
-wget $GITMINE/squid.conf -qO $CONFDIR/squid.conf
-wget $GITMINE/docker.yaml -qO- | docker stack up -c - dnsx
+spin='-\|/'
+str='Removing possible errors...'
+cnl='Cancelled.'
+fin='Finished.'
+trap 'printf "\b\b$cnl%$(( ${#str}-${#cnl}+1 ))s\n";exit' INT
+while [[ `docker network ls | grep dnsx` ]]; do
+i=$(( (i+1) %4 ))
+printf "\b${str}${spin:$i:1} \r"
+sleep .1
+done
+printf "\b\b$fin%$(( ${#str}-${#fin}+1 ))s\n"
+cat docker.yaml | docker stack up -c - dnsx
 docker service update $(docker service ls | grep squid | cut -d ' ' -f 1) --args $sqx
