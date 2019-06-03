1291a00b874043d04831b78cab80e272e2f30e9b
Dexter Cellona Banawon <28706220+X-DCB@users.noreply.github.com>
2019-06-03 08:50:44 +0800

    Add files via upload

--- docker-dns.sh
+++ docker-dns.sh
@@ -36,7 +36,7 @@ sleep .1
 done
 printf "\b\b$fin%$(( ${#str}-${#fin}+1 ))s\n"
 mkdir $CONFDIR 2> $DNUL
-wget $GITMINE/sniproxy.conf -qO $CONFDIR/sniproxy.conf
+wget $GITMINE/sniproxy.conf -qO- | echo "$([[ `cat /etc/network/interfaces` =~ 'inet6 static' ]] && sed -e 's/ipv4_only/ipv6_first/g')" > $CONFDIR/sniproxy.conf 
 wget $GITMINE/dnsmasq.conf -qO $CONFDIR/dnsmasq.conf
 wget $GITMINE/squid.conf -qO $CONFDIR/squid.conf
 wget $GITMINE/sni-dns.conf -qO $CONFDIR/sni-dns.conf
