3c23d1a446b490f5d580cb5cc9a184783f6b7d8f
Dexter Cellona Banawon <28706220+X-DCB@users.noreply.github.com>
2019-06-17 13:25:38 +0800

    Add files via upload

--- docker-dns.sh
+++ docker-dns.sh
@@ -6,7 +6,7 @@ read -p "Location: [SG] " vloc
 vloc=${vloc:-SG}
 [ $vloc = SG ] && vlocx=squidx-serv
 if [[ ! `type -P docker` ]]; then
-if [[ `uname -v` =~ Debian ]]; then
+if [ -f /etc/debian* ]; then
 apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
